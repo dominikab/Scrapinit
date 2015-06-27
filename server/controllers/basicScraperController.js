b62a1452153bb035a07849f4a1114125183a311e
Dominika <bienkowska.dominika@gmail.com>
2015-06-27 16:25:50 -0700

    [Feature] added img processing prior to tesseract to improve OCR accuracy

--- server/controllers/basicScraperController.js
+++ server/controllers/basicScraperController.js
@@ -19,15 +19,15 @@ module.exports = {
     var urlWithoutHTTP = url.substring(url.indexOf("://") + 3)  // handle http AND https protocols
     urlWithoutHTTP = urlWithoutHTTP.replace(/[?/.=]/g, '_');    // change weird characters to underscore
 
-    webshot(url, '../client/assets/' + userId + '/' + urlWithoutHTTP + '-preview.jpg', function(err) {
+    webshot(url, '../client/assets/' + userId + '/' + urlWithoutHTTP + '-preview.png', function(err) {
       // screenshot now saved to google.png// screenshot now saved to hello_world.png
-      cb('assets/' + userId + '/' + urlWithoutHTTP + '-preview.jpg');
+      cb('assets/' + userId + '/' + urlWithoutHTTP + '-preview.png');
     });
   },
 
   cropImg: function(url, crop, compare, cb) {
 
-    var filepath = url.substr(0, url.length - 12) + ((compare) ? '-compare.jpg' : '.jpg');
+    var filepath = url.substr(0, url.length - 12) + ((compare) ? '-compare.png' : '.png');
 
      gm('../client/' + url).crop(crop.w, crop.h, crop.x, crop.y)
       .write('../client/' + filepath, function(err){
@@ -38,11 +38,21 @@ module.exports = {
   },
 
   imagetotext: function(img, cb) {
+    // image filtering
+    var current = '../client/'+img;
+    var filteredImg = "../client/"+img.slice(0,img.length-4)+".tiff";
+
+   var imgFil = gm(current).type('grayscale').enhance().unsharp(6.8, 1.0, 2.69, 0).resize(1200,2000).write(filteredImg, function (err) {
+  // ...
+    }); 
+
+    console.log('filteredImg',filteredImg);
     //nodecr.process('../client/assets/1/www_amazon_com_Down-Rabbit-Hole-Adventures-Cautionary_dp_0062372106_ref_zg_bsnr_books_2.jpg',function(err, text) {
-    nodecr.process('../client/' + img,function(err, text) {
+    nodecr.process(filteredImg, function(err, text) {
         if(err) {
             console.error(err);
         } else {
+            console.log('text from tesseract', text);
             cb(text);
         }
     });
